9119d9b28bc75601c07f04c3b4aba02efa04a66f
felipemarinhodev <felipmarinho@gmail.com>
2022-07-22 08:01:37 -0700

    add ui-modules

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { sum } from "@gabrielmarinho/utils/math/sum";
+import { Text } from "@gabrielmarinho/design-system/components/Text";
 
 export default function HomeScreen() {
   return (
     <main>
-      <h1>Home</h1>
-      <p>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">
         Importando modulo local: @gabrielmarinho/utils/math/sum sum(1,1) ={" "}
         {sum(1, 1)}
-      </p>
+      </Text>
     </main>
   );
 }
