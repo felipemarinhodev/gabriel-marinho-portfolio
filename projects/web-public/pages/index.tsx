ac724f8f1f8775fe46461273494a6cd38bf13ea0
felipemarinhodev <felipmarinho@gmail.com>
2022-07-22 07:26:22 -0700

    initial workspaces

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { sum } from "@gabrielmarinho/utils/math/sum";
 
 export default function HomeScreen() {
   return (
     <main>
       <h1>Home</h1>
+      <p>
+        Importando modulo local: @gabrielmarinho/utils/math/sum sum(1,1) ={" "}
+        {sum(1, 1)}
+      </p>
     </main>
   );
 }
