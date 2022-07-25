067648ec663b33454ded3583f7d52158401eaccb
felipemarinhodev <felipmarinho@gmail.com>
2022-07-24 22:48:03 -0700

    add jest-styled-components and update import testing-library-dom

--- packages/design-system/components/Text/index.test.tsx
+++ packages/design-system/components/Text/index.test.tsx
@@ -1,17 +1,19 @@
 import { Text } from ".";
 import { customRender } from "@gabrielmarinho/test-commons/react-testing-library";
 import { screen } from "@gabrielmarinho/test-commons/dom-testing-library";
-import "@testing-library/jest-dom";
 
 const render = customRender();
 
 describe("<Text />", () => {
   it("renders h1 tag", () => {
     render(<Text tag="h1">Simple Text</Text>);
-    expect(
-      screen.getByRole("heading", {
-        name: /simple text/i,
-      })
-    ).toBeInTheDocument();
+    const wrapper = screen.getByRole("heading", {
+      name: /simple text/i,
+    });
+    expect(wrapper).toBeInTheDocument();
+
+    expect(wrapper).toHaveStyle({
+      fontFamily: "sans-serif",
+    });
   });
 });
